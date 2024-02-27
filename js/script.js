3a0f7c8680ec619aebe0e734e44381be450b9adf
Jose <josecarlos099916@gmail.com>
2024-02-27 14:53:34 -0300

    Conclusão da lógica do bot

--- js/script.js
+++ js/script.js
@@ -252,14 +252,16 @@ function computerPlay() {
     filled = 0
 
     for(let i = 0; i < boxes.length; i++) {
-        let randomNumber = Math.floor(Math.random * 5)
+        let randomNumber = Math.floor(Math.random() * 5)
 
+        // PREENCHE CASO O FILHO ESTEJA VAZIO
         if(boxes[i].childNodes[0] == undefined) {
             if(randomNumber <= 1) {
                 boxes[i].appendChild(cloneO) 
                 counter++
                 break
             }
+        // CHECA QUANTAS ESTÃO PREENCHIDAS
         } else {
             filled++
         }
